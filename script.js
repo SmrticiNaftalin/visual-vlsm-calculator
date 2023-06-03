33829b0fae2e82c102bb14b22735befc7c7bdfdf
SmrticiNaftalin <105565698+SmrticiNaftalin@users.noreply.github.com>
2023-06-03 16:16:50 +0200

    added more info to the output table

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 number_of_subnets = 0
 subnets = []
-let ip = document.getElementById("network_input").value;
 
-//add_subnet("A", 121);
-//add_subnet("B", 62);
-//add_subnet("C", 2);
-//add_subnet("D", 7);
-//add_subnet("E", 14);
+
+//add_subnet("A", 120);
+//add_subnet("B", 60);
+//add_subnet("C", 20);
+//add_subnet("D", 14);
+//add_subnet("E", 6);
+//add_subnet("F", 2);
+//add_subnet("G", 2);
 
 function save_network() {
+
+    ip = document.getElementById("network_input").value;
     document.getElementById("saved_network").innerText = ip
+    
     mask = parseInt(ip.split("/")[1])
     if ((mask >= 24) && (mask <=30)) {
         console.log(mask);
@@ -66,7 +71,7 @@ function halve(rectangle_object) {
     var half_1_max = half_1_min + new_capacity - 1;
 
     var half_2_min = half_1_max + 1;
-    var half_2_max = half_2_min + new_capacity;
+    var half_2_max = half_2_min + new_capacity - 1;
 
     return [{"square": half_1,
              "capacity": new_capacity,
@@ -85,17 +90,20 @@ function halve(rectangle_object) {
 
 function add_subnet(name, size) {
 
+document.querySelector('input[name="sub_name"]').value = "";
+document.querySelector('input[name="sub_size"]').value = "";
+
 const table = document.getElementById("table");
 
 let row = document.createElement("tr");
 
 let name_element = document.createElement("td");
-name_element.classList.add("subnet_table_name")
-name_element.innerText = name
+name_element.classList.add("subnet_table_name");
+name_element.innerText = name;
 
 let hosts = document.createElement("td");
-hosts.classList.add("subnet_table_hosts")
-hosts.innerText = size
+hosts.classList.add("subnet_table_hosts");
+hosts.innerText = size;
 
 table.appendChild(row);
 row.appendChild(name_element);
@@ -107,6 +115,29 @@ subnets.push({"name": name,
             })
 
 number_of_subnets++
+
+calculate();
+
+}
+
+function create_table_header(text, parent){
+
+table_header = document.createElement("th");
+table_header.classList.add("subnet_header");
+table_header.innerText = text;
+
+parent.appendChild(table_header);
+
+}
+
+function create_table_cell(text, class_name, parent) {
+    
+table_cell = document.createElement("td");
+table_cell.classList.add(class_name);
+table_cell.innerHTML = text;
+
+parent.appendChild(table_cell);
+
 }
 
 function write_subnets_and_masks(subnets) {
@@ -115,40 +146,47 @@ function write_subnets_and_masks(subnets) {
 
     console.log(subnets);
     const results = document.getElementById("result");
+
     results.innerHTML = "";
+
+    create_table_header("název", results);
+    create_table_header("IP sítě", results);
+    create_table_header("/ maska", results);
+    create_table_header("maska", results);
+    create_table_header("wildcard", results);
+    create_table_header("1. platná IP", results);
+    create_table_header("pos. platná IP", results);
+
+    const alternating_background = {0 : "",
+                                    1 : "#3badcc",
+    }
+
     for (k in subnets) {
     
     subnet_row = document.createElement("tr");
     subnet_row.classList.add("subnet_row")
+    subnet_row.style = "background-color:" + alternating_background[subnets[k]["order"] % 2] + "; font-weight: bold;"
 
-    subnet_name = document.createElement("td");
-    subnet_name.classList.add("subnet_table_name")
-    subnet_name.innerText = subnets[k].name;
+    create_table_cell(subnets[k].name, "subnet_table_name", subnet_row);
 
     ip_without_last_byte = ip.split("/")[0].split(".");
     ip_without_last_byte.pop();
     ip_without_last_byte = ip_without_last_byte.join(".") + ".";
+    ip_without_last_byte = "<ip>"+ip_without_last_byte+"</ip>";
     
-    console.log("IP", ip_without_last_byte);
-
-    subnet_ip = document.createElement("td");
-    subnet_ip.classList.add("subnet_table_address")
-    subnet_ip.innerText = ip_without_last_byte + String(subnets[k].start_ip);
+    create_table_cell(ip_without_last_byte + String(subnets[k].start_ip), "subnet_table_address", subnet_row);
+    create_table_cell(" / "+subnets[k]["mask_/"], "subnet_table_mask", subnet_row);
+    create_table_cell("255.255.255." + subnets[k]["mask"], "subnet_table_mask_full", subnet_row);
+    create_table_cell("0.0.0." + (255 - subnets[k]["mask"]), "subnet_table_wildcard_mask", subnet_row);
     
-    subnet_mask = document.createElement("td");
-    subnet_mask.classList.add("subnet_table_mask")
-    subnet_mask.innerText = " / "+subnets[k]["mask_/"];
-
-    subnet_mask_full = document.createElement("td");
-    subnet_mask_full.classList.add("subnet_table_mask_full")
-    subnet_mask_full.innerText = "." + subnets[k]["mask"];
-
+    //create_table_cell(ip_without_last_byte + String(parseInt(subnets[k]["start_ip"] + 1)), "subnet_table_first", subnet_row);
+    create_table_cell("." + String(parseInt(subnets[k]["start_ip"] + 1)), "subnet_table_first", subnet_row);
+    create_table_cell("." + parseInt(subnets[k]["end_ip"] - 1), "subnet_table_last", subnet_row);
     
-    subnet_row.appendChild(subnet_name);
-    subnet_row.appendChild(subnet_ip);
-    subnet_row.appendChild(subnet_mask);
-    //subnet_row.appendChild(subnet_mask_full);
+    blank_row = document.createElement("tr");
+    blank_row.classList.add("blank_row")
 
+    //results.appendChild(blank_row);
     results.appendChild(subnet_row);
     }
 }
@@ -194,13 +232,15 @@ function generate_square(subnets_and_sizes) {
 
     final_subnets = [];
 
-    const size_to_mask = {  256: "24",
-                            128: "25",
-                            64: "26",
-                            32: "27",
-                            16: "28",
-                            8: "29",
-                            4: "30",  }
+    const size_to_masks = { 256:   ["24", 0],
+                            128:   ["25", 128],
+                            64:    ["26", 192],
+                            32:    ["27", 224],
+                            16:    ["28", 240],
+                            8:     ["29", 248],
+                            4:     ["30", 252],
+                        }
+
 
     let rectangles = [{ "square": document.getElementById("vlsm_square"),
                         "capacity": 256,
@@ -217,16 +257,15 @@ function generate_square(subnets_and_sizes) {
             
             final_subnets.push({"name": subnets_and_sizes[0].name,
                                 "start_ip": rectangles[0]["min"],
-                                "mask": rectangles[0]["capacity"],
-                                "mask_/": size_to_mask[rectangles[0]["capacity"]],
+                                "end_ip": rectangles[0]["max"],
+                                "mask": size_to_masks[rectangles[0]["capacity"]][1],
+                                "mask_/": size_to_masks[rectangles[0]["capacity"]][0],
                                 "order": subnets_and_sizes[0]["order"]
                                 });
             console.log(rectangles[0])
 
             subnets_and_sizes.shift();
             rectangles.shift();
-            
-            
 
     }  else if ((subnets_and_sizes[0].size) < rectangles[0]["capacity"]){
             //console.log(rectangles[0].square, rectangles)
